fca887a2c2d7c8b8fbf8a446e47f03537fb8d9e8
litolite <litovec.89@gmail.com>
2020-12-31 20:58:27 +0500

    Накидал набросок прототипа

--- app/src/main/java/com/deathtospies/mymovieinfo/MovieDetailsActivity.kt
+++ app/src/main/java/com/deathtospies/mymovieinfo/MovieDetailsActivity.kt
@@ -3,7 +3,7 @@ package com.deathtospies.mymovieinfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
