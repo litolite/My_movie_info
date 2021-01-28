37fd08ee3d9c02aefc652e27c1490d43f884c68f
litolite <litovec.89@gmail.com>
2021-01-29 01:52:03 +0500

    set MainActivity.kt to view binding feature

--- app/src/main/java/com/deathtospies/mymovieinfo/FragmentMoviesList.kt
+++ app/src/main/java/com/deathtospies/mymovieinfo/FragmentMoviesList.kt
@@ -39,14 +39,8 @@ class FragmentMoviesList : Fragment() {
 
     companion object {
         /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment FragmentMoviesList.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(): FragmentMoviesList {
             return FragmentMoviesList()
